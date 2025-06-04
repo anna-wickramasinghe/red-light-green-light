9578ae2afd10dd2b4c7356eb8f7926dc6e6619f1
anna-wickramasinghe <nprawickramasinghe@gmail.com>
2025-06-04 23:30:58 +0530

    add move player functionality and randomized the red light green light toggle.

--- index.js
+++ index.js
@@ -1,17 +1,34 @@
-let isGreen = Math.floor(Math.random()*2)+1; 
-let randomSeconds = Math.floor(Math.random()*6)+1; 
+function toggleLight(){
+    let isGreen = Math.floor(Math.random()*2)+1; 
+    let randomSeconds = Math.floor(Math.random()*6)+1; 
 
-document.querySelector(".img").setAttribute("src", `./images/doll${isGreen}.png`);
+    document.querySelector(".img").setAttribute("src", `./images/doll${isGreen}.png`);
 
 
-if (isGreen==1){
-    document.querySelector("h1").innerHTML="🟢Green Light";
-    document.querySelector("p").innerHTML="Run";
-    document.querySelector("button").innerHTML="Click to Stop";
+    if (isGreen==1){
+        document.querySelector("h1").innerHTML="🟢Green Light";
+        document.querySelector("p").innerHTML="Run";
+        document.querySelector("button").innerHTML="Click to Stop";
+    }
+    else{
+        document.querySelector("h1").innerHTML="🔴Red Light";
+        document.querySelector("p").innerHTML="HOLD";
+        document.querySelector("button").innerHTML="Click to Move";
+    }
+    setTimeout(toggleLight, randomSeconds*1000)
+
 }
-else{
-    document.querySelector("h1").innerHTML="🔴Red Light";
-    document.querySelector("p").innerHTML="HOLD";
-    document.querySelector("button").innerHTML="Click to Move";
-    document.querySelector(".imgPlayer").setAttribute("src","./images/stand.png");
-}
+
+toggleLight();
+
+let toggleRunning = false;
+
+document.getElementById("movePlayer").addEventListener("click", function(){
+    toggleRunning = !toggleRunning;
+    if(toggleRunning){
+        document.querySelector(".imgPlayer").setAttribute("src","./images/run.gif");
+    } else {
+        document.querySelector(".imgPlayer").setAttribute("src","./images/stand.png");
+    }
+
+});
